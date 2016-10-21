22efc7489f675d3493f3ccb53ea1df7f0baf60c8
Andy Hume <andyhu@brandwatch.com>
2016-10-21 13:51:01 +0100

    Refactor to improve flow of main edit function

--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ var jsyaml = require('js-yaml');
 
 function get (YAML, path) {
   if (typeof YAML !== 'string' || typeof path !== 'string') {
-    throw new Error('get() takes two string args. The yaml string and a path string.')
+    throw new Error('function takes two string args. The yaml string and a path string.')
   }
   return _.get(jsyaml.load(YAML), path);
 };
 
 function edit (YAML, path, newValue) {
-  if (typeof YAML !== 'string' || typeof path !== 'string') {
-    throw new Error('edit() takes two string args. The yaml string and a path string.')
-  }
-
   var currentValue = get(YAML, path);
   if (!currentValue) {
     throw new Error(
@@ -22,41 +18,30 @@ function edit (YAML, path, newValue) {
   var pathArr = _.castPath(path);
   var finalProp = pathArr[pathArr.length - 1];
 
-  // If finalProp is a number, then check if it was an index
-  if (!isNaN(finalProp)) {
-    var l = finalProp.length + 2;
-    var finalPropIsArrayIndex = (path.slice(-l) === '[' + finalProp + ']');
-  }
-
-  var re;
-
-  if (finalPropIsArrayIndex) {
-    re = new RegExp('- *' + currentValue);
-    return YAML.replace(re,  '- ' + newValue);
-  }
-
-  re = new RegExp(finalProp + ' *: *' + currentValue, 'g');
-
   var matchToken = 'matchToken' + Math.random().toString(36);
   var matchIndex = 0;
 
-  tokenisedYAML = YAML.replace(re, function (match, p1, offset) {
+  var reMatchAllKeyValuePairs = new RegExp(finalProp + ' *: *' + currentValue, 'g');
+  var reMatchAllKeyTokenPairs = new RegExp(finalProp + ' *: *' + matchToken + '\\d+', 'gm');
+
+  var targetIsArrayMember = Array.isArray(_.get(jsyaml.load(YAML), pathArr.slice(0, -1)));
+
+  if (targetIsArrayMember) {
+    // TODO: Deal with multiple duplicate values.
+    return YAML.replace(new RegExp('- *' + currentValue),  '- ' + newValue);
+  }
+
+  tokenisedYAML = YAML.replace(reMatchAllKeyValuePairs, function () {
     return finalProp + ': ' + matchToken + matchIndex++;
   });
 
-  // Find token that matches original path.
-  var tokenisedValue = get(tokenisedYAML, path);
+  var reMatchTargetKeyToken = new RegExp(finalProp + ' *: *' + get(tokenisedYAML, path));
 
-  // Update tokenisedValue to the newValue
-  re = new RegExp(finalProp + ' *: *' + tokenisedValue);
-  tokenisedYAML = tokenisedYAML.replace(re, finalProp + ': ' + newValue);
-
-  // Switch all remaining matchTokens back to their original values.
-  re = new RegExp(finalProp + ' *: *' + matchToken + '\\d+', 'gm')
-  var finalYAML = tokenisedYAML.replace(re, finalProp + ': ' + currentValue);
-
-  return finalYAML;
-
+  return tokenisedYAML
+    // Switch matching token to the new value
+    .replace(reMatchTargetKeyToken, finalProp + ': ' + newValue)
+    // Switch all remaining matchTokens back to their original values
+    .replace(reMatchAllKeyTokenPairs, finalProp + ': ' + currentValue);
 }
 
 module.exports = {
